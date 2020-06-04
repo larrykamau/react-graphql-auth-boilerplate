2d8632580c6df337f1b79e0442bdf96dd1e42be8
larrykamau <lawrencekamau5@gmail.com>
2020-06-04 19:38:14 +0300

    Init
    changes to toml

--- src/containers/App.test.js
+++
@@ -1,9 +0,0 @@
-import App from "./App";
-import React from "react";
-import renderer from "react-test-renderer";
-
-it("renders correctly", () => {
-  const component = renderer.create(<App />);
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
